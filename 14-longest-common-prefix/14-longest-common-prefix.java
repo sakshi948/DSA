885c2af9e46c353063fb5320909230047923b4c3
sakshi948 <54503255+sakshi948@users.noreply.github.com>
2022-01-31 19:05:30 +0530

    Time: 7 ms (23.75%), Space: 42 MB (6.16%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -1,39 +1,20 @@
 class Solution {
-    public String longestCommonPrefix(String[] strs) 
-    {
-      
-        int minlength=500;
-        for(int i=0;i<strs.length;i++)
-        {
-          minlength=Math.min(strs[i].length(),minlength)  ;
-        }
-        boolean flag=true;
-        String ans[]=new String[1];
-        ans[0]="";
-        prefix(ans,strs,0,minlength,flag);
-            return ans[0];
+    public String longestCommonPrefix(String[] strs) {
+        int minsize = Integer.MAX_VALUE;
+        for(var s : strs) minsize = Math.min(minsize,s.length());
         
-    }
-    public static String[] prefix(String[] ans,String[]strs,int curr,int minlength,boolean flag)
-    {
-        if(curr>=minlength)
-        {
-            return ans;
-        }
-        char ch=strs[0].charAt(curr);
-     
-        for(int i=0;i<strs.length;i++)
-        {
-            if(strs[i].charAt(curr)!=ch)
-            {
-                flag=false;
-                break;
+        //System.out.println(minsize);
+        int  flag = 0;
+        String ans = "";
+        for(int i = 0; i< minsize ; i++){
+            char c = strs[0].charAt(i);
+            for(int j = 0 ; j<strs.length ; j++){
+                if(strs[j].charAt(i)==c) flag = 1;
+                else {flag = 0;break;}
             }
-        }
-        if(flag)
-        {
-            ans[0]+=ch;
-            prefix(ans,strs,curr+1,minlength,flag);
+            if(flag==0) break;
+            if(flag==1) {ans+=c;}
+            
         }
         return ans;
     }
